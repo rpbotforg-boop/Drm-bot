478157e39dac3061c12e4d6b6c4b1d5bf48c8da3
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-01 23:34:17 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,25 +1,26 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24712525"))
-API_HASH = environ.get("API_HASH", "6487082b6952f0d981f5f23d97c32992")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8062100326:AAG8BdWEBGfgdFqMKgxM03M_JjBRuyIIZLc")
+API_ID = int(environ.get("API_ID", "22447622"))
+API_HASH = environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8387440827:AAHTwkJTiLb5Wc-Rru407_0d_2UsaYazy4Y")
 
-OWNER = int(environ.get("OWNER", "6334323103"))
-CREDIT = environ.get("CREDIT", "💫『 𝒟𝒾𝓋𝓎𝒶𝓃𝓈𝒽 𝓈𝒽𝓊𝓀𝓁𝒶 』💫")
+OWNER = int(environ.get("OWNER", "777756062"))
+CREDIT = environ.get("CREDIT", "💫『 Ravindra Patel』💫")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6334323103').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '777756062').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6334323103').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '777756062').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
 
